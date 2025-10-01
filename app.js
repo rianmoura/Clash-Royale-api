298a79f5ce94976a77a952a6e7c3cb3784a7c34b
Rian Moura <163539263+rianmoura@users.noreply.github.com>
2025-10-01 07:43:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const axios = require("axios");
-const path = require("path");
 
 const app = express();
-const PORT = 3000;
 
-const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImMwMmQzZDNhLTllMGYtNDYxNy04ZjliLTI5MDE4NTg5NTZlYyIsImlhdCI6MTc1OTMxNDg2Miwic3ViIjoiZGV2ZWxvcGVyL2ZjMzdmNzIzLThlNTEtNTcxMS05MjUxLWZlOTZlYzUzMTk2MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NC4yMzMuMTUxLjI3Il0sInR5cGUiOiJjbGllbnQifV19.um28nc-Q2Fyd0sHvFYFTcshATxNqvLz_nKGUmW8BxkOEMr0mMbubalIV06Yzz1yU21t3e_yzuZKDhbJLz82-4Q";
+// Rota de teste
+app.get("/", (req, res) => {
+  res.send("API Clash Royale rodando no Render! ⚔️👑");
+});
 
-app.use(express.static(path.join(__dirname, "public")));
-
+// Rota para buscar jogador pela tag
 app.get("/player/:tag", async (req, res) => {
-  const tag = encodeURIComponent(req.params.tag); // garante %23
+  const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImMwMmQzZDNhLTllMGYtNDYxNy04ZjliLTI5MDE4NTg5NTZlYyIsImlhdCI6MTc1OTMxNDg2Miwic3ViIjoiZGV2ZWxvcGVyL2ZjMzdmNzIzLThlNTEtNTcxMS05MjUxLWZlOTZlYzUzMTk2MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0NC4yMzMuMTUxLjI3Il0sInR5cGUiOiJjbGllbnQifV19.um28nc-Q2Fyd0sHvFYFTcshATxNqvLz_nKGUmW8BxkOEMr0mMbubalIV06Yzz1yU21t3e_yzuZKDhbJLz82-4Q"; // coloque seu token aqui
+  const tag = req.params.tag.toUpperCase();
+
   try {
     const response = await axios.get(
-      `https://api.clashroyale.com/v1/players/${tag}`,
+      `https://api.clashroyale.com/v1/players/%23${tag}`,
       {
         headers: {
           Authorization: `Bearer ${API_KEY}`,
@@ -21,6 +23,7 @@ app.get("/player/:tag", async (req, res) => {
         },
       }
     );
+
     res.json(response.data);
   } catch (error) {
     if (error.response) {
@@ -31,7 +34,8 @@ app.get("/player/:tag", async (req, res) => {
   }
 });
 
+// O Render obriga usar process.env.PORT
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
-
