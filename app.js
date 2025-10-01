c3cd1e6289c5892b478328549cccfa21b91e432d
Rian Moura <163539263+rianmoura@users.noreply.github.com>
2025-10-01 08:30:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
 
@@ -17,5 +18,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 
