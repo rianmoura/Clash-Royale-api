ceb2d4efb42075d4734b8ca1178c1ade3bb29137
Rian Moura <163539263+rianmoura@users.noreply.github.com>
2025-10-01 08:57:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ const path = require("path");
 const app = express();
 
 
-// 🔑 Coloque seu token aqui (ou use variáveis de ambiente .env)
+
 const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjM3MjRmYTIxLTY4ZGQtNGJhZC1hYzlhLTc2YmZiYjM1NTE1NiIsImlhdCI6MTc1OTMxNzQwMiwic3ViIjoiZGV2ZWxvcGVyL2ZjMzdmNzIzLThlNTEtNTcxMS05MjUxLWZlOTZlYzUzMTk2MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIzNC4yMTEuMjAwLjg1IiwiNDQuMjMzLjE1MS4yNyIsIjM1LjE2MC4xMjAuMTI2Il0sInR5cGUiOiJjbGllbnQifV19.NKxyWP2Nrr_qjbEWxGp8OaWdQ4dqcfY34yM0lGriP3HjrsjPHIWHyqEytiguNg5dqCiUq7FB5yIRqu0jySz4Wg";
+
+
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/player/:tag", async (req, res) => {
-  const tag = encodeURIComponent(req.params.tag);
+  const tag = encodeURIComponent(req.params.tag); // garante %23
   try {
     const response = await axios.get(
-      `https://proxy.royaleapi.dev/v1/players/${tag}`, // <<< usando proxy
+      `https://api.clashroyale.com/v1/players/${tag}`,
       {
         headers: {
-          Authorization: `Bearer ${API_KEY}`, // <<< token vai aqui
+          Authorization: `Bearer ${API_KEY}`,
           Accept: "application/json",
         },
       }
@@ -36,6 +38,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 
 
