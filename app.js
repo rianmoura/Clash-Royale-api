f706e882f814bf2e9e7da8c11609317aadf3dd7b
Rian Moura <163539263+rianmoura@users.noreply.github.com>
2025-10-01 08:26:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,35 +3,13 @@ const axios = require("axios");
 
 const app = express();
 
-// Rota de teste
+
+// Serve arquivos estáticos da pasta public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Se alguém acessar /, manda o index.html
 app.get("/", (req, res) => {
-  res.send("API Clash Royale rodando no Render! ⚔️👑");
-});
-
-// Rota para buscar jogador pela tag
-app.get("/player/:tag", async (req, res) => {
-  const API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjM3MjRmYTIxLTY4ZGQtNGJhZC1hYzlhLTc2YmZiYjM1NTE1NiIsImlhdCI6MTc1OTMxNzQwMiwic3ViIjoiZGV2ZWxvcGVyL2ZjMzdmNzIzLThlNTEtNTcxMS05MjUxLWZlOTZlYzUzMTk2MCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIzNC4yMTEuMjAwLjg1IiwiNDQuMjMzLjE1MS4yNyIsIjM1LjE2MC4xMjAuMTI2Il0sInR5cGUiOiJjbGllbnQifV19.NKxyWP2Nrr_qjbEWxGp8OaWdQ4dqcfY34yM0lGriP3HjrsjPHIWHyqEytiguNg5dqCiUq7FB5yIRqu0jySz4Wg"; // coloque seu token aqui
-  const tag = req.params.tag.toUpperCase();
-
-  try {
-    const response = await axios.get(
-      `https://api.clashroyale.com/v1/players/%23${tag}`,
-      {
-        headers: {
-          Authorization: `Bearer ${API_KEY}`,
-          Accept: "application/json",
-        },
-      }
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      res.status(500).json({ error: error.message });
-    }
-  }
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // O Render obriga usar process.env.PORT
@@ -39,4 +17,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
